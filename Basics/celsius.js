f44eaeaedc1ab8d437691b98267106db0de065b9
irangareddy <itsmerr17@gmail.com>
2020-04-21 13:55:09 +0530

    JS Basics Initial Commit

---
+++ Basics/celsius.js
@@ -0,0 +1,27 @@
+var score = 100
+
+score = 216
+
+const bonus = 20
+
+var finalScore = (score + bonus) * 1.4
+
+var complex = ((4+4)*5)/4
+
+var moduleOpertor = 100%2
+
+// console.log(finalScore)
+// console.log(complex)
+// console.log(moduleOpertor)
+
+/* Calculating Temperature in celsius */
+
+let tempInFahrenheit = 100
+
+// do calculation   Formula : (32°F − 32) × 5/9 = 0°C
+
+
+let tempInCelsius = (tempInFahrenheit-32) * 5/9 
+
+console.log(tempInCelsius+'℃')
+
---
+++ Basics/variables.js
@@ -0,0 +1,27 @@
+let name = 'Ranga'
+let score = 100
+
+/* No worries without ; also because JavaScript allows */
+
+console.log(name,score)
+
+let bonus = 20.7
+let totalScore = score + bonus
+
+console.log(name,totalScore)
+
+let firstName = 'Elon'
+
+let lastName = 'Musk'
+
+console.log(firstName + '-' + lastName)
+
+firstName = 'Jonny'
+
+let fullName = firstName+'<-->'+lastName
+
+console.log(fullName)
+
+const initialName = 'Ranga'
+/*you cant change the constant value once you have declared initialName = 'Reddy'*/
+console.log(initialName)
